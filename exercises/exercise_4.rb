78c28afe9dee314f0b6a3594d5bff109d0453f5d
stanw0ng <user.stanley@gmail.com>
2023-05-01 17:41:46 -0400

    Add filter for high earning women's clothing stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,10 +11,18 @@ Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens
 Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
-@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+@mens_stores = Store.where(mens_apparel: true)
 
-# should only be one store since Gastown was deleted
+# should return 3 stores since Gastown was deleted
 puts "Number of men's stores: #{@mens_stores.size}"
 @mens_stores.each do |store|
+  puts "Name: #{store.name}, Annual revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: 1000000..Float::INFINITY)
+
+# should return 1 store
+puts "Number of women's stores: #{@womens_stores.size}"
+@womens_stores.each do |store|
   puts "Name: #{store.name}, Annual revenue: #{store.annual_revenue}"
 end
