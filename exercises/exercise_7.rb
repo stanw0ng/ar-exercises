8a55e45f18cad8663ab460ae0b261b93bdfdfa44
stanw0ng <user.stanley@gmail.com>
2023-05-01 19:10:48 -0400

    add validators to Stores and Employees

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+# order of arguments here matches the order when created BUT order doesnt matter as long as they match the columns
+new_store = Store.create(name: store_name, annual_revenue: 10000000, mens_apparel: true, womens_apparel: false)
+
+if new_store.errors.any?
+  puts "Errors occurred while saving the store:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+else
+  puts "Store was saved successfully!"
+end
