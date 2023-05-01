d7f01d2213eb21f0882c03443bd558fe83098ffc
stanw0ng <user.stanley@gmail.com>
2023-05-01 18:07:33 -0400

    add calculations for stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue of all stores: $#{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue).round
+puts "Average revenue of each store: $#{average_revenue}"
+
+@big_buck_stores = Store.where(annual_revenue: 1000000..Float::INFINITY).size
+puts "There are #{@big_buck_stores} stores making $1M or more"
