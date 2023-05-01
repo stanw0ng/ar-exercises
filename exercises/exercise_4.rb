4c53bbe5d98d9dd561a4180e60d614a570bc3749
stanw0ng <user.stanley@gmail.com>
2023-05-01 17:23:40 -0400

    Add more stores and select specific men's clothing stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,14 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+
+# should only be one store since Gastown was deleted
+puts "Number of men's stores: #{@mens_stores.size}"
+@mens_stores.each do |store|
+  puts "Name: #{store.name}, Annual revenue: #{store.annual_revenue}"
+end
